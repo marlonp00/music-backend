69c26dfff98e9b639aae8b9f0bf30653057de011
Marlon Perini <marlon.perini@gmail.com>
2023-01-14 18:07:45 +0100

    sadscfd

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
+  const { host, port, database, user, password } = parse("DATABASE_URL");
 
   return {
     connection: {
